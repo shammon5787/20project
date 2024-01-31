b1697a5ca37b47b76eb46f4f1da3c2ae99cd2087
Sadam <sadam.bsit@gmail.com>
2024-01-31 19:12:09 +0500

    changes here

--- src/components/menu/ImageCategory.jsx
+++ src/components/menu/ImageCategory.jsx
@@ -11,19 +11,20 @@ const ImageCategory = () => {
     //     setImages(upDateCategory);
     // }
 
-    const searchGategory = (catElem) =>{
+    const searchGategory = (catElem) =>{    
         const updateitem = Menu.filter((curEem)=>{
             return curEem.category === catElem;
         });
-        setImages(updateitem);
+        setImages(updateitem );
     }
+    
 
     return (
         < >
             <div className='mainBtns' style={{ textAlign: 'center', justifyContent: 'space-between', alignItems: 'center', marginTop: '10px' }}>
                 <button style={{ cursor: 'pointer', backgroundColor: 'blue', padding: '3px', marginLeft: '20px', borderRadius: '5px' }} onClick={()=>searchGategory('Fruits')}>Fruuit</button>
-                <button style={{ cursor: 'pointer', backgroundColor: 'blue', padding: '3px', marginLeft: '20px', borderRadius: '5px' }}>Dinner</button>
-                <button style={{ cursor: 'pointer', backgroundColor: 'blue', padding: '3px', marginLeft: '20px', borderRadius: '5px' }}>Lunch</button>
+                <button style={{ cursor: 'pointer', backgroundColor: 'blue', padding: '3px', marginLeft: '20px', borderRadius: '5px' }} onClick={()=>searchGategory('Shirts')}>Dinner</button>
+                <button style={{ cursor: 'pointer', backgroundColor: 'blue', padding: '3px', marginLeft: '20px', borderRadius: '5px' }} onClick={()=>searchGategory('Drinks')}>Lunch</button>
                 <button style={{ cursor: 'pointer', backgroundColor: 'blue', padding: '3px', marginLeft: '20px', borderRadius: '5px' }}>Breakfast</button>
                 <button style={{ cursor: 'pointer', backgroundColor: 'blue', padding: '3px', marginLeft: '20px', borderRadius: '5px' }}>All</button>
             </div>
